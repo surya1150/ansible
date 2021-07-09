ab465979e3214b301d1fd56137bff1941668c0d3
surya1150 <kotha.surya1150@gmail.com>
2021-07-09 14:49:44 +0000

    docker installation steps

---
+++ docker-install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+sudo apt-get update
+sudo apt-get install apt-transport-https     ca-certificates     curl     gnupg     lsb-release
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+sudo docker images
